83dfa151741174d795afe703fcf5b1f35480e9a9
zeynelhg <zeynelabidinhug@gmail.com>
2015-06-24 16:28:46 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Launcher
 // @namespace   AposLauncher
-// @include     http://agar.io/
+// @include     http://agariopvp.com/
 // @version     2.81
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
